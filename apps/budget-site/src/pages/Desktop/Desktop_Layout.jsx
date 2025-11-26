52c7d7b5f6dba24445f30ead21fdae073854e9c5
Mark Wade <wade.mark.a@gmail.com>
2025-11-26 09:17:15 -0700

    BUDGET | DESKTOP: ADDED OPTIONS TO LEFT PANEL WITH ICOMS. THE DIVE LOGHT UP WHEN HOVERED. NO INTERACTIVE THINGS YET.

--- apps/budget-site/src/pages/Desktop/Desktop_Layout.jsx
+++ apps/budget-site/src/pages/Desktop/Desktop_Layout.jsx
@@ -2,9 +2,36 @@
 export function Desktop_Layout(){
     return(
         <div className="budget_desktop_layout">
+
+
             <div className="budget_left_panel">
-                <p>Left Panel</p>
+                <div>
+                    <img src="./dashboard.png"/>
+                    <h6>Dashboard</h6>
+                </div>
+                <div>
+                    <img src="./budget.png"/>
+                    <h6>Budget</h6>
+                </div>
+                <div>
+                    <img src="./transaction.png"/>
+                    <h6>Transactions</h6>
+                </div>
+                <div>
+                    <img src="./debt.png"/>
+                    <h6>Debt</h6>
+                </div>
+                <div>
+                    <img src="./goals.png"/>
+                    <h6>Goals</h6>
+                </div>
+                <div>
+                    <img src="./settings.png"/>
+                    <h6>Settings</h6>
+                </div>
             </div>
+
+
             <div className="budget_right_panel">
                 <h2>Item View Port</h2>
             </div>

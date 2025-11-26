f0c117ca3d3d572a254b6807a9cffd2919e2033a
Mark Wade <wade.mark.a@gmail.com>
2025-11-26 15:08:51 -0700

    BUTTON || BUDGET |CAT: BUTTON: ADDED SOME ANIMATIONS FOR WHEN INTERATING WITH// BUTDEGT CAT: STARTED ADDING ELELEMTNS

--- apps/budget-site/src/pages/Desktop/Cat_desktop.jsx
+++ apps/budget-site/src/pages/Desktop/Cat_desktop.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Input, Button} from '@wade/ui'
 
 function Cat_desktop({selected}) {
+  const [newItem, setNewItem] = useState('')
+  const [newNote, setNewNote] = useState('')
+
+
+
   return (
     <div className={selected === "cat" ? "budget_desktop_selected" : "budget_desktop_not_selected"} >
-        Budget Categories
+        <div className='cat_desktop_wrapper'>
+
+          <div className='cat_desktop_add_items'>
+              <Input labelText={'Add New Item'} type={'text'} id={'add_item'} value={newItem} change={(e)=>setNewItem(e.target.value)}/>
+              <Input labelText={'Add New note'} type={'text'} id={'add_note'} value={newNote} change={(e)=>setNewNote(e.target.value)}/>
+              <Button text={'Add Item'} button_type={'primary'} />
+          </div>
+
+        </div>
     </div>
   )
 }
